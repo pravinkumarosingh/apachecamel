f458d131e2727485c46e7ee0d04eef6e19ff7199
pravinkumarosingh <pravinkumarosingh@gmail.com>
2024-08-25 10:52:06 +0530

    using choice and simple language in apache camel

--- apachecamelpractice-a/apachecamelpractice-a/src/main/java/com/sugarglider/apachecamelpracticea/routes/b/MyFileRouter.java
+++ apachecamelpractice-a/apachecamelpractice-a/src/main/java/com/sugarglider/apachecamelpracticea/routes/b/MyFileRouter.java
@@ -3,13 +3,28 @@ package com.sugarglider.apachecamelpracticea.routes.b;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class MyFileRouter extends RouteBuilder{
 
 	@Override
 	public void configure() throws Exception {
 		// TODO Auto-generated method stub
-		from("file:files/input").log("${body}").to("file:files/output");
+		//from("file:files/input").log("${body}").to("file:files/output");
+
+		from("file:files/input")
+				.log("${body}")
+				.routeId("File Input Route")
+				.transform().body(String.class)
+				.choice()
+				.when(simple("${file:ext} ends with 'xml'"))
+					.log("XML FILE")
+				.when(simple("${body} contains 'John'"))
+					.log("Not an xml file but contains John")
+				.otherwise()
+					.log("Not a xml file")
+				.end()
+				.log("${messageHistory} ${headers.CamelFileLastModified}")
+				.to("file:files/output");
 	}
 
 }
--- apachecamelpractice-a/apachecamelpractice-a/src/main/java/com/sugarglider/apachecamelpracticea/routes/c/RestApiConsumerRouter.java
+++ apachecamelpractice-a/apachecamelpractice-a/src/main/java/com/sugarglider/apachecamelpracticea/routes/c/RestApiConsumerRouter.java
@@ -3,7 +3,7 @@ package com.sugarglider.apachecamelpracticea.routes.c;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class RestApiConsumerRouter extends RouteBuilder {
 
     @Override
